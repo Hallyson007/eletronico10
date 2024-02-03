6b01455d1da5d7528fc82f8c5d54c825c95d980b
hallyson007 <hallysonmartins@gmail.com>
2024-02-03 03:02:30 -0300

    ok banco agora

--- cadastroproduto.php
+++ cadastroproduto.php
@@ -2,13 +2,13 @@
 
 include 'conexao.php';
 
-$nome = $_POST['produto']; // Altere 'produto' para 'nome'
+$produto = $_POST['produto'];
 $descricao = $_POST['descricao'];
 $categoria = $_POST['categoria'];
 $estoque = $_POST['estoque'];
 $preco = $_POST['preco'];
 
-$sql = "INSERT INTO `produtos` (`nome`, `descricao`, `categoria`, `estoque`, `preco`) VALUES ('$nome','$descricao','$categoria','$estoque','$preco')"; // Altere 'produto' para 'nome'
+$sql = "INSERT INTO `produtos` (`produto`, `descricao`, `categoria`, `estoque`, `preco`) VALUES ('$produto','$descricao','$categoria','$estoque','$preco')";
 
 $inserir = mysqli_query($conexao,$sql);
 
